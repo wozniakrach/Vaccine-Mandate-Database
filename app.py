63b63b6d2a867fcf6c2d2a70e063b1b992704e45
Rachel Wozniak <wozniakr@oregonstate.edu>
2022-03-03 16:14:12 -0600

    mismatched bracket

--- app.py
+++ app.py
@@ -17,32 +17,34 @@ mysql = MySQL(app)
 # Routes
 @app.route('/')
 def root():
-    #query = "SELECT * FROM diagnostic;"
-    #cur = mysql.connection.cursor()
-    #cur.execute(query)
-    #results = cur.fetchall()
-    #return results[0]
+    # query = "SELECT * FROM diagnostic;"
+    # cur = mysql.connection.cursor()
+    # cur.execute(query)
+    # results = cur.fetchall()
+    # return results[0]
     return render_template("home.html")
+
 
 @app.route('/employees', methods=["POST", "GET"])
 def employees():
-  #  if request.method == "POST":
-   #     if request.form.get("employee-submit"):
+    #  if request.method == "POST":
+    #     if request.form.get("employee-submit"):
     #        fname = request.form["fname"]
-     #       lname = request.form["lname"]
-      #      birthdate = request.form["birthdate"]
-       #     termed = request.form["termed"]
-        #    site = request.form["site"]
-         #   exemption = request.form["exemption"]
-          #  if exemption == "N/A":
-           #     exemption = None
-            #insert_query = "INSERT INTO Employees (first_name, last_name, birthdate, termed, site_id, exemption_id) VALUES (%s, %s, %s, %s, %s, %s)" % (fname, lname, birthdate, termed, site, exemption)
-          #  print(insert_query)
+    #       lname = request.form["lname"]
+    #      birthdate = request.form["birthdate"]
+    #     termed = request.form["termed"]
+    #    site = request.form["site"]
+    #   exemption = request.form["exemption"]
+    #  if exemption == "N/A":
+    #     exemption = None
+    # insert_query = "INSERT INTO Employees (first_name, last_name, birthdate, termed, site_id, exemption_id) VALUES (%s, %s, %s, %s, %s, %s)" % (fname, lname, birthdate, termed, site, exemption)
+    #  print(insert_query)
     select_query = "SELECT * FROM Employees;"
     cursor = mysql.connection.cursor()
     cursor.execute(select_query)
     data = cursor.fetchall()
     return render_template("employees.j2", employees_table=data)
+
 
 @app.route('/worksites')
 def worksites():
@@ -52,13 +54,16 @@ def worksites():
     data = cursor.fetchall()
     return render_template("worksites.j2", worksites_table=data)
 
+
 @app.route('/exemptions')
 def exemptions():
     return render_template("exemptions.html")
 
+
 @app.route('/vaccines')
 def vaccines():
     return render_template("vaccines.html")
+
 
 @app.route('/employees_vaccines')
 def employees_vaccines():
@@ -67,6 +72,5 @@ def employees_vaccines():
 
 # Listener
 if __name__ == "__main__":
-
-    #Start the app on port 3000, it will be different once hosted
+    # Start the app on port 3000, it will be different once hosted
     app.run(port=32999, debug=True)
