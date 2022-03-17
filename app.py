9b0de97fc61653e93175d1f1e2f4d278ee3b30c1
Rachel Wozniak <wozniakr@oregonstate.edu>
2022-03-17 15:54:41 -0700

    employee null exemption_id for update form

--- app.py
+++ app.py
@@ -87,15 +87,22 @@ def edit_employees():
             site_id = request.form["site_id"]
             exemption_id = request.form["exemption_id"]
             employee_id = request.form["employee_id"]
+
             # account for null exemption_id
             if exemption_id == -1:
-                exemption_id = None
+                query = "UPDATE Employees SET first_name=%s, last_name=%s, birthdate=%s, termed=%s, site_id=%s, " \
+                        "WHERE employee_id=%s;"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (first_name, last_name, birthdate, termed, site_id))
+                mysql.connection.commit()
 
-            query = "UPDATE Employees SET first_name=%s, last_name=%s, birthdate=%s, termed=%s, site_id=%s, " \
+            # no null values
+            else:
+                query = "UPDATE Employees SET first_name=%s, last_name=%s, birthdate=%s, termed=%s, site_id=%s, " \
                     "exemption_id=%s WHERE employee_id=%s;"
-            cur = mysql.connection.cursor()
-            cur.execute(query, (first_name, last_name, birthdate, termed, site_id, exemption_id, employee_id))
-            mysql.connection.commit()
+                cur = mysql.connection.cursor()
+                cur.execute(query, (first_name, last_name, birthdate, termed, site_id, exemption_id, employee_id))
+                mysql.connection.commit()
 
             # redirect back to people page after we execute the update query
             return redirect("/employees")
